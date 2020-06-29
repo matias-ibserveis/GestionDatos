d7ad5ab4415d471164efe3d666ec1fa29602aadf
matias-ibserveis <matias@ibserveis.com>
2020-06-29 15:54:14 +0200

    Revert "inicio contenteditabel"
    This reverts commit 06a2d470e317eced2d8dc83e379e877f057c6f0d.

--- src/codigo/flash.js
+++ src/codigo/flash.js
@@ -1,11 +1,11 @@
 export default function flash(element) {
 	requestAnimationFrame(() => {
 		element.style.transition = 'none';
-		element.style.color = '#99f03d';
-		
+		element.style.color = '#5598D0';
+		element.style.backgroundColor = '##f0f0f0;';
 
 		setTimeout(() => {
-			element.style.transition = 'color 1s';
+			element.style.transition = 'color 1s, background 1s';
 			element.style.color = '';
 			element.style.backgroundColor = '';
 		});
